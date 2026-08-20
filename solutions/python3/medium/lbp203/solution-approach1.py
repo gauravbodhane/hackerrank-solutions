555a44e71db5f08fcc5c38df6a060ed7262f2eaf
Gaurav Bodhane <149650679+gauravbodhane@users.noreply.github.com>
2026-08-20 08:32:32 +0530

    ✅ LBP203 [python3] — approach1

---
+++ solutions/python3/medium/lbp203/solution-approach1.py
@@ -0,0 +1,21 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/contests/41dec30av2022/challenges/lbp203/problem?isFullScreen=true
+# Problem     LBP203
+# Difficulty  Medium
+# Subdomain   N/A
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-08-20, 08:32 a.m.
+# ──────────────────────────────────────────────────
+
+
+a=[]
+for i in range(3):
+    a.append([int (i) for i in input().split()])
+s = 0
+for i in range(3):
+    for j in range(3):
+        if a[i][j]%2==0:
+            s = s+a[i][j]
+print(s)
