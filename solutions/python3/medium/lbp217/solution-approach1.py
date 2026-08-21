540d244a2b8d5ab80d7e841b028c2fd803434d96
Gaurav Bodhane <149650679+gauravbodhane@users.noreply.github.com>
2026-08-21 20:14:57 +0530

    ✅ LBP217 [python3] — approach1

---
+++ solutions/python3/medium/lbp217/solution-approach1.py
@@ -0,0 +1,20 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/contests/matrixtest/challenges/lbp217/problem?isFullScreen=true
+# Problem     LBP217
+# Difficulty  Medium
+# Subdomain   N/A
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-08-21, 08:14 p.m.
+# ──────────────────────────────────────────────────
+
+# Enter your code here. Read input from STDIN. Print output to STDOU
+a = []
+
+for i in range(3):
+    a.append([int(x) for x in input().split()])
+for i in range(3):
+    for j in range(3):
+        print(a[j][i],end=' ')
+    print()
