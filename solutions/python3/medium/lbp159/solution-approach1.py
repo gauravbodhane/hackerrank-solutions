ed78aa9d5c4715dc8f474aacd6688f2e7d0c6588
Gaurav Bodhane <149650679+gauravbodhane@users.noreply.github.com>
2026-08-29 09:22:18 +0530

    ✅ LBP159 [python3] — approach1

---
+++ solutions/python3/medium/lbp159/solution-approach1.py
@@ -0,0 +1,14 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/contests/41dec30av2022/challenges/lbp159/problem?isFullScreen=true
+# Problem     LBP159
+# Difficulty  Medium
+# Subdomain   N/A
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-08-29, 09:22 a.m.
+# ──────────────────────────────────────────────────
+
+n = int(input())
+L = [int(i) for i in input().split()]
+print(str(0 not in L).lower())
