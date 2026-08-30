11cbe3b510ea2cbeb09216c3f894194ef5a25737
Gaurav Bodhane <149650679+gauravbodhane@users.noreply.github.com>
2026-08-30 14:17:23 +0530

    ✅ LBP164 [python3] — approach1

---
+++ solutions/python3/medium/lbp164/solution-approach1.py
@@ -0,0 +1,17 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/contests/41dec30av2022/challenges/lbp164/problem?isFullScreen=true
+# Problem     LBP164
+# Difficulty  Medium
+# Subdomain   N/A
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-08-30, 02:17 p.m.
+# ──────────────────────────────────────────────────
+
+n = int(input())
+l = [int(i) for i in input().split()]
+print(l[1],end=' ')
+for i in range(1,n-1):
+    print(l[i-1]*l[i+1], end=' ')
+print(l[n-2])
