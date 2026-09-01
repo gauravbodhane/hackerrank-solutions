9f6c7a5c366c5bbcaef12e7bf02bc7980110d0ae
Gaurav Bodhane <149650679+gauravbodhane@users.noreply.github.com>
2026-09-01 10:25:59 +0530

    ✅ LBP171 [python3] — approach1

---
+++ solutions/python3/medium/lbp171/solution-approach1.py
@@ -0,0 +1,16 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/contests/41dec30av2022/challenges/lbp171/problem?isFullScreen=true
+# Problem     LBP171
+# Difficulty  Medium
+# Subdomain   N/A
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-09-01, 10:25 a.m.
+# ──────────────────────────────────────────────────
+
+L= [
+    "break",'case','continue','default','defer','else','for','func','goto','if','map','range','return','struct','type','var'
+]
+s= input()
+print('true' if s in L else 'false')
