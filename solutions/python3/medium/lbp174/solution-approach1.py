f172d17ba5fb9e9c7ee9f59fc5af9fce486eef08
Gaurav Bodhane <149650679+gauravbodhane@users.noreply.github.com>
2026-09-02 08:17:03 +0530

    ✅ LBP174 [python3] — approach1

---
+++ solutions/python3/medium/lbp174/solution-approach1.py
@@ -0,0 +1,15 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/contests/41dec30av2022/challenges/lbp174/problem?isFullScreen=true
+# Problem     LBP174
+# Difficulty  Medium
+# Subdomain   N/A
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-09-02, 08:17 a.m.
+# ──────────────────────────────────────────────────
+
+n = input( )
+for ch in n:
+    print(ord(ch)- ord('a'),end='')
+    
