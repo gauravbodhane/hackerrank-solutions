1aa06b0c4c75f30476676a62135ba14cbe57593b
Gaurav Bodhane <149650679+gauravbodhane@users.noreply.github.com>
2026-09-02 08:11:28 +0530

    ✅ LBP173 [python3] — approach1

---
+++ solutions/python3/medium/lbp173/solution-approach1.py
@@ -0,0 +1,13 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/contests/41dec30av2022/challenges/lbp173/problem?isFullScreen=true
+# Problem     LBP173
+# Difficulty  Medium
+# Subdomain   N/A
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-09-02, 08:11 a.m.
+# ──────────────────────────────────────────────────
+
+
+print(int(input(),17))
