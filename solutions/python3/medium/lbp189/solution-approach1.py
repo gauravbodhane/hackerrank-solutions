70da53f6f494ec385347732f1e5a16596b1fdf14
Gaurav Bodhane <149650679+gauravbodhane@users.noreply.github.com>
2026-09-11 17:19:07 +0530

    ✅ LBP189 [python3] — approach1

---
+++ solutions/python3/medium/lbp189/solution-approach1.py
@@ -0,0 +1,19 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/contests/41dec30av2022/challenges/lbp189/problem?isFullScreen=true
+# Problem     LBP189
+# Difficulty  Medium
+# Subdomain   N/A
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-09-11, 05:19 p.m.
+# ──────────────────────────────────────────────────
+
+n = input()
+
+g = n[::-1]
+
+g1 = int(g)
+n2 = int(n) 
+print(abs(g1-n2))
+
