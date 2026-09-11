db101b8ec7555c53dae1694d1bc955323f9ca759
Gaurav Bodhane <149650679+gauravbodhane@users.noreply.github.com>
2026-09-11 17:06:14 +0530

    ✅ LBP187 [python3] — approach1

---
+++ solutions/python3/medium/lbp187/solution-approach1.py
@@ -0,0 +1,15 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/contests/41dec30av2022/challenges/lbp187/problem?isFullScreen=true
+# Problem     LBP187
+# Difficulty  Medium
+# Subdomain   N/A
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-09-11, 05:06 p.m.
+# ──────────────────────────────────────────────────
+
+n = input()
+g = (n[-1],n[-2])
+
+print(*g,sep=",")
