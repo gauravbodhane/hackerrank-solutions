bbfa9d1fc7e4ec6072e55e0e51b76dc4f1e01687
Gaurav Bodhane <149650679+gauravbodhane@users.noreply.github.com>
2026-09-13 13:05:56 +0530

    ✅ LBP196 [python3] — approach1

---
+++ solutions/python3/medium/lbp196/solution-approach1.py
@@ -0,0 +1,14 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/contests/41dec30av2022/challenges/lbp196/problem?isFullScreen=true
+# Problem     LBP196
+# Difficulty  Medium
+# Subdomain   N/A
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-09-13, 01:05 p.m.
+# ──────────────────────────────────────────────────
+
+b,e,m = (int(i) for i in input().split())
+
+print(int(b**e%m))
