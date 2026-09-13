c9b4991129c436f26b2e9736a2ce4c1bf07ca5f1
Gaurav Bodhane <149650679+gauravbodhane@users.noreply.github.com>
2026-09-13 14:00:11 +0530

    ✅ LBP200 [python3] — approach1

---
+++ solutions/python3/medium/lbp200/solution-approach1.py
@@ -0,0 +1,18 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/contests/41dec30av2022/challenges/lbp200/problem?isFullScreen=true
+# Problem     LBP200
+# Difficulty  Medium
+# Subdomain   N/A
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-09-13, 02:00 p.m.
+# ──────────────────────────────────────────────────
+
+n = int(input())
+l =[int(i) for i in input().split()]
+k = int(input())
+s = 0
+for i in l:
+    s =s+i%k
+print(s)
