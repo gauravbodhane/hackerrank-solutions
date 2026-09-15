c2e78fb7f4519b123f3fbc7db1500e6e135930b0
Gaurav Bodhane <149650679+gauravbodhane@users.noreply.github.com>
2026-09-15 13:25:44 +0530

    ✅ LBP255 [python3] — approach1

---
+++ solutions/python3/medium/lbp255/solution-approach1.py
@@ -0,0 +1,14 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/contests/41dec30av2022/challenges/lbp255/problem?isFullScreen=true
+# Problem     LBP255
+# Difficulty  Medium
+# Subdomain   N/A
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-09-15, 01:25 p.m.
+# ──────────────────────────────────────────────────
+
+n = input()
+
+print(int(n)-int(n[::-1]))
