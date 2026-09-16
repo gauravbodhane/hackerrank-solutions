ebbb64d53577d74590692d1de5c09d43bae371f1
Gaurav Bodhane <149650679+gauravbodhane@users.noreply.github.com>
2026-09-16 13:36:41 +0530

    ✅ LBP259 [python3] — approach1

---
+++ solutions/python3/medium/lbp259/solution-approach1.py
@@ -0,0 +1,25 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/contests/41dec30av2022/challenges/lbp259/problem?isFullScreen=true
+# Problem     LBP259
+# Difficulty  Medium
+# Subdomain   N/A
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-09-16, 01:36 p.m.
+# ──────────────────────────────────────────────────
+
+n = input()
+for i  in n:
+    if i in 'aeiou':
+        print(i , end='')
+    else:
+        ch = i  
+        if ch =='z':
+
+            ch ='a' 
+        ch = chr(ord(ch)+1)
+        if ch in 'aeiou':
+            print(chr(ord(ch)+1),end='')
+        else:
+            print(ch,end='')
