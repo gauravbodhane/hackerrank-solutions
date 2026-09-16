5c75cc8ee2fbb2d6664b504998d9f213c35996e4
Gaurav Bodhane <149650679+gauravbodhane@users.noreply.github.com>
2026-09-16 14:58:45 +0530

    ✅ LBP261 [python3] — approach1

---
+++ solutions/python3/medium/lbp261/solution-approach1.py
@@ -0,0 +1,14 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/contests/41dec30av2022/challenges/lbp261/problem?isFullScreen=true
+# Problem     LBP261
+# Difficulty  Medium
+# Subdomain   N/A
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-09-16, 02:58 p.m.
+# ──────────────────────────────────────────────────
+
+n = int(input())
+s = bin(n) 
+print(s.count('1'))
