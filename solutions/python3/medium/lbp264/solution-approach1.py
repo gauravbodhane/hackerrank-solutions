ab176fed06357e60fadd75eb00f6550d0c5a1a5f
Gaurav Bodhane <149650679+gauravbodhane@users.noreply.github.com>
2026-09-17 14:01:42 +0530

    ✅ LBP264 [python3] — approach1

---
+++ solutions/python3/medium/lbp264/solution-approach1.py
@@ -0,0 +1,13 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/contests/41dec30av2022/challenges/lbp264/problem?isFullScreen=true
+# Problem     LBP264
+# Difficulty  Medium
+# Subdomain   N/A
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-09-17, 02:01 p.m.
+# ──────────────────────────────────────────────────
+
+n , m = (int(i) for i in input().split())
+print((n**m)%10)
