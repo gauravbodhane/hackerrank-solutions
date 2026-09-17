99aef61005237a63b6fdda584d90d7370f079780
Gaurav Bodhane <149650679+gauravbodhane@users.noreply.github.com>
2026-09-17 14:22:20 +0530

    ✅ LBP267 [python3] — approach1

---
+++ solutions/python3/medium/lbp267/solution-approach1.py
@@ -0,0 +1,19 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/contests/41dec30av2022/challenges/lbp267/problem?isFullScreen=true
+# Problem     LBP267
+# Difficulty  Medium
+# Subdomain   N/A
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-09-17, 02:22 p.m.
+# ──────────────────────────────────────────────────
+
+n = int(input())
+i = 1
+while True:
+    if i * i>= n:
+        print(i*i) 
+        break
+    i = i +1 
+    
