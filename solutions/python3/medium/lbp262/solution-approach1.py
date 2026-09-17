45a17a3964b17b3d45e5ab1bcd26a2658669eb0c
Gaurav Bodhane <149650679+gauravbodhane@users.noreply.github.com>
2026-09-17 13:44:01 +0530

    ✅ LBP262 [python3] — approach1

---
+++ solutions/python3/medium/lbp262/solution-approach1.py
@@ -0,0 +1,13 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/contests/41dec30av2022/challenges/lbp262/problem?isFullScreen=true
+# Problem     LBP262
+# Difficulty  Medium
+# Subdomain   N/A
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-09-17, 01:43 p.m.
+# ──────────────────────────────────────────────────
+
+n = int(input())
+print('true' if n**3%10==n else 'false') 
