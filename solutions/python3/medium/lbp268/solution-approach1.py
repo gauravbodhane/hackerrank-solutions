825cf0d96daa70e52ae6b876cbf397285aa8c5c7
Gaurav Bodhane <149650679+gauravbodhane@users.noreply.github.com>
2026-09-17 14:24:49 +0530

    ✅ LBP268 [python3] — approach1

---
+++ solutions/python3/medium/lbp268/solution-approach1.py
@@ -0,0 +1,15 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/contests/41dec30av2022/challenges/lbp268/problem?isFullScreen=true
+# Problem     LBP268
+# Difficulty  Medium
+# Subdomain   N/A
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-09-17, 02:24 p.m.
+# ──────────────────────────────────────────────────
+
+a = int(input())
+b = int(input())
+
+print(abs((a+b)-(a*b)))
