ab3fbd19fc962cb003dfc5a72f62453744594951
Gaurav Bodhane <149650679+gauravbodhane@users.noreply.github.com>
2026-09-17 13:51:25 +0530

    ✅ LBP263 [python3] — approach1

---
+++ solutions/python3/medium/lbp263/solution-approach1.py
@@ -0,0 +1,14 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/contests/41dec30av2022/challenges/lbp263/problem?isFullScreen=true
+# Problem     LBP263
+# Difficulty  Medium
+# Subdomain   N/A
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-09-17, 01:51 p.m.
+# ──────────────────────────────────────────────────
+
+n = input() 
+s = int(input())
+print(n[s-1] if s<len(n) else '-1')    
